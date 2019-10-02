cb193158eda6a60b6cdafd90830778eb7c143844
Sanganu <kppkmsanu@gmail.com>
2019-10-02 15:26:04 -0600

    Updates

---
+++ precache-manifest.c3b3f2c84721120f7418691c3486e7d4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79f21f80cdb40daaadf543e3d2660e51",
+    "url": "/index.html"
+  },
+  {
+    "revision": "745ab0c2c3a059d76a79",
+    "url": "/static/css/main.3e461a6e.chunk.css"
+  },
+  {
+    "revision": "66128c22c26bdcc6bda3",
+    "url": "/static/js/2.0bcd6b4b.chunk.js"
+  },
+  {
+    "revision": "745ab0c2c3a059d76a79",
+    "url": "/static/js/main.f4e7bcfc.chunk.js"
+  },
+  {
+    "revision": "1a2a8841a24b9986bc05",
+    "url": "/static/js/runtime-main.7473fdf2.js"
+  }
+]);
