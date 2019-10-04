5f81f60e4513a9fbcbe59d5276208e2b11037ec5
Sanganu <kppkmsanu@gmail.com>
2019-10-04 09:06:37 -0600

    intialchgs

---
+++ src/components/Header.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import styled from 'styled-components';
+import "./style.css";
+
+// const Button = styled.button`
+//     font-family: 
+//     font-size: 2rem;
+//     border: 5px
+//     border-radius: 10px;
+// `;
+
+
+
+function Header ()  {
+  return(<div className="headerline">
+         <h2>Memory GAME</h2>
+      </div>)
+}
+
+export default Header;
+
+
---
+++ src/components/cards.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+import "./style.css";
+
+const cards = () =>{ return(<div className="cardsstyle">
+       {this.props.value}
+        
+    </div>)
+}
+
+module.exports = cards;
