f5947d1074f5f99807689e51d9481953ff783cb7
Sanganu <kppkmsanu@gmail.com>
2020-07-07 09:04:00 -0600

    Added .

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,7 @@
 import React from 'react';
-// import styled from 'styled-components';
-// import "./style.css";
-
-// const Button = styled.button`
-//     font-family: 
-//     font-size: 2rem;
-//     border: 5px
-//     border-radius: 10px;
-// `;
-
-
 
 function Header ()  {
-  return(<header className="headerline #0d47a1">
+  return(<header className="headerline">
          <nav>
          <h2>Memory GAME</h2>
         </nav>
