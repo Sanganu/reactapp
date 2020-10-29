d1c79335044cbb13f1c70df3dc43310d52462be4
Sanganu <kppkmsanu@gmail.com>
2020-10-29 12:33:11 -0600

    Change nav bar color

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 function Header ()  {
-  return(<header className="headerline">
+  return(<header className="headerline mt-2 pt-2">
          <nav>
          <h2>Memory GAME</h2>
         </nav>
